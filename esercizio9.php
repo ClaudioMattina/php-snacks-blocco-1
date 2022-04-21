3ba784cbc1262150505929fc943f09e67ea113db
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-21 12:36:35 +0200

    comando terzo esercizio

---
+++ esercizio9.php
@@ -0,0 +1,17 @@
+<?php 
+/* Partendo da questa stringa $string = "c,i,n,q,u,e"; 
+trasforma la stringa in un array con un elemento per carattere,
+
+	$array = ['c','i','n','q','u','e'];
+
+infine crea un array associativo della forma:
+	
+	$result = [
+		'c' => 'c',
+		'i' => 'i',
+		'n' => 'n',
+		'q' => 'q',
+		'u' => 'u',
+		'e' => 'e'
+	]; */
+?>
